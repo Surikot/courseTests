6bc5dcf4d64ba6041047b3485ff33ec304dfe171
Suriya <spm@haraba.ru>
2021-12-10 16:17:15 +0300

    init

---
+++ courseTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using FsCheck.Experimental;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace courseTest
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://harabadealer.ru/";
+            driver.FindElement(By.XPath("/html/body/app-root/app-start-page/div/header/div/div/div/div[2]/button[2]")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+             driver.Quit();
+             driver = null;
+        }
+    }
+}
